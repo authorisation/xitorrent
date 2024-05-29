de581dbf13d4f18e48300f14f021380454bb6dca
auth <64337177+authorisation@users.noreply.github.com>
2024-05-29 15:00:52 +0200

    refactor: make -t optional

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #define VERSION "1.0.0"
 
 void print_help(const char *progname) {
-    printf("Usage: [-h] -t text -i file.torrent -o output.png\n");
+    printf("Usage: [-h] -t title -i file.torrent -o output.png\n");
     printf("  -h                            Display this help message\n");
     printf("  -t <text>                     Torrent title\n");
     printf("  -i <input.torrent>            Input torrent file\n");
@@ -20,7 +20,7 @@ void print_help(const char *progname) {
 }
 
 int main(int argc, char *argv[]) {
-    const char *text = NULL;
+    const char *title = NULL;
     const char *info = NULL;
     const char *input_filename = NULL;
     const char *output_filename = NULL;
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
                 print_help(argv[0]);
                 exit(EXIT_SUCCESS);
             case 't':
-                text = optarg;
+                title = optarg;
                 break;
             case 'i':
                 input_filename = optarg;
@@ -49,20 +49,20 @@ int main(int argc, char *argv[]) {
         }
     };
 
-    if (!text || !input_filename || !output_filename) {
+    if (!input_filename || !output_filename) {
         fprintf(stderr, "Error: missing required options\n");
         print_help(argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    int width = 494;
-    int height = 55;
-    cairo_surface_t *surface = init_image(width, height);
-
+    //Torrent parser
     TorrentInfo torrent_info;
     parseTorrent(input_filename, &torrent_info);
 
-    draw_title(output_filename, text, info, surface);
+    //Image drawing
+    cairo_surface_t *surface = init_image(494, 55);
+    title = title ? title : torrent_info.name;
+    draw_title(output_filename, title, info, surface);
     draw_body(output_filename, torrent_info.num_seeds, torrent_info.num_peers, 0, torrent_info.file_count, torrent_info.total_size, torrent_info.num_trackers, 0, surface);
 
     return 0;
